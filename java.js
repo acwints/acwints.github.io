f715d392ad2bfb755c08401b7c23ec0646fc6f07
Andrew Winter <ac.winter@outlook.com>
2020-05-26 16:12:23 -0400

    Color Game Upload

---
+++ java.js
@@ -0,0 +1,20 @@
+var num = 1
+
+while( num <= 20) {
+	if(num % r === 0) {
+		console.log(num)
+	}
+	num++;
+}
+
+;
+
+
+var num = 5
+
+while(num < 50) {
+	if( num % 3 === 0 && num % 5 === 0) {
+		console.log(num)
+	}
+	num++
+}
